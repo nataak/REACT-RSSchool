4cd131bb6a2e863fa9a016dd5f25938749e05e09
Nataak <nataak848@gmail.com>
2023-10-29 01:11:18 +0400

    feat: add bootstap stilys

--- React-Components/src/Components/Main/MainPage.tsx
+++ React-Components/src/Components/Main/MainPage.tsx
@@ -1,12 +1,33 @@
-import { Component} from "react";
-class MainPage extends Component{
+import { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Col, Row } from 'react-bootstrap';
+import './MainPage.css';
+
+class MainPage extends Component {
   render() {
-    return (
-      <>
-      <div>Hello</div></>
-    )
+    return(
+      <><Navbar
+        className="navbar"
+      >
+        <InputGroup className="mb-3 search">
+          <Form.Control
+            placeholder="Search"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2" />
+          <Button variant="success" id="button-addon2">
+            Button
+          </Button>
+        </InputGroup>
+      </Navbar>
+          <Row className='row'>
+            <Col className='col'>1 of 1fffffffffffffffffffffffffffffffffffffff</Col>
+          </Row>
+        </>
+    );
   }
 }
 
 export default MainPage;
-
