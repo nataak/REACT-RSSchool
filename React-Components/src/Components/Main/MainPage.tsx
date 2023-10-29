498ba7f4e0cee4bd310dec997b6afcf78de4bc1d
Nataak <nataak848@gmail.com>
2023-10-29 14:10:24 +0400

    feat: add fetch data

--- React-Components/src/Components/Main/MainPage.tsx
+++ React-Components/src/Components/Main/MainPage.tsx
@@ -6,41 +6,45 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import { Col, Row } from 'react-bootstrap';
 import './MainPage.css';
 
-
 interface IState {
   films: [];
-  }
+}
 
-class MainPage extends Component <object, IState>{
+class MainPage extends Component<object, IState> {
   constructor(props: object) {
     super(props);
-    this.state={
-      films: []
-    }
+    this.state = {
+      films: [],
+    };
   }
-    render() {
-    return(
+  componentDidMount() {
+    fetch('https://swapi.dev/api/films/?format=json')
+      .then((response) => response.json())
+      .then((results) => this.setState(() => {
+        return { films: results };
+      }, () => {
+        console.log(this.state)
+      }));
+  }
+  render() {
+    return (
       <>
-        
-       <div>
-          <Navbar
-        className="navbar top-section">
-          <InputGroup className="mb-3 search"
-                        >
-          <Form.Control
-            placeholder="Search" type="text"/>
-          <Button variant="success" id="button-addon2">
-            Search
-          </Button>
-        </InputGroup>
-                    
-                </Navbar>
-                <Row className='row'>
-            <Col className='col'>1 of 1fffffffffffffffffffffffffffffffffffffff</Col>
-          </Row> 
-             
-            </div>
-        </>
+        <div>
+          <Navbar className="navbar top-section">
+            <InputGroup className="mb-3 search">
+              <Form.Control placeholder="Search" type="text" />
+              <Button variant="success" id="button-addon2">
+                Search
+              </Button>
+            </InputGroup>
+          </Navbar>
+          <Row className="row">
+            <Col className="col">
+              1 of 1fffffffffffffffffffffffffffffffffffffff
+            </Col>
+          </Row>
+        </div>
+      </>
     );
   }
 }
